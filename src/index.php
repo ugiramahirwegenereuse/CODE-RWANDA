5b61b600d70216d1885a2a8749dc44bb55e5a794
ugiramahirwegenerese <ugiramahirwegenereuse@gmail.com>
2025-11-04 10:13:21 +0200

    index page created

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ride Sharing System</title>
+</head>
+<body>
+    <h1>Welcome to CODERWANDA Ride Sharing</h1>
+    <div>
+        <a href="registration.php">Register</a> | 
+        <a href="login.php">Login</a>
+    </div>
+</body>
+</html>
